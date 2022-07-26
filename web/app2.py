717cc5b6d831ee2a5abc58b39f78a2e548ee9ba7
eragefe <georgios@gdis.se>
2022-07-26 14:19:48 +0200

    new

--- web/app2.py
+++ web/app2.py
@@ -17,24 +17,6 @@ def index():
          input = f.read()
     return render_template('app2.html', vol=vol, input=input, filter=filter)
 
-@app.route('/wifi')
-def wifi():
-    wifi_ap_array = scan_wifi_networks()
-
-    return render_template('wifi.html', wifi_ap_array = wifi_ap_array)
-
-@app.route('/manual_ssid_entry')
-def manual_ssid_entry():
-    return render_template('manual_ssid_entry.html')
-
-@app.route('/save_credentials', methods = ['GET', 'POST'])
-def save_credentials():
-    ssid = request.form['ssid']
-    wifi_key = request.form['wifi_key']
-    create_wpa_supplicant(ssid, wifi_key)
-    os.system('bash /tmp/wifi.tmp')
-    return redirect('/')
-
 @app.route("/volume2", methods = ['GET', 'POST'])
 def volume2():
     if request.method == 'POST':
@@ -128,6 +110,10 @@ def power():
 def confirm():
     return render_template('update.html')
 
+@app.route('/working')
+def working():
+    return render_template('working.html')
+
 @app.route('/reboot', methods = ['GET', 'POST'])
 def reboot():
     os.system('bash -c "sleep 1; reboot"&')
@@ -135,8 +121,10 @@ def reboot():
 
 @app.route('/update', methods = ['GET', 'POST'])
 def update():
-    os.system('bash -c "sleep 1; updateroot"&')
-    return redirect('/')
+      os.system('bash -c "sleep 1; updateroot"&')
+      return redirect('/working')
+      time.sleep (1.5)
+      return redirect('/')
 
 @app.route('/no', methods = ['GET', 'POST'])
 def no():
@@ -166,31 +154,6 @@ def stop():
 def next():
     os.system('mpc next')
     return redirect('/')
-
-######## FUNCTIONS ##########
-
-def create_file(a):
-
-    temp_conf_file = open('/root/vol', 'w')
-    temp_conf_file.write('' + a + '')
-    temp_conf_file.close
-
-def scan_wifi_networks():
-    iwlist_raw = subprocess.Popen(['iwlist','wlan0','scan'], stdout=subprocess.PIPE)
-    ap_list, err = iwlist_raw.communicate()
-    ap_array = []
-
-    for line in ap_list.decode('utf-8').rsplit('\n'):
-        if 'ESSID' in line:
-            ap_ssid = line[27:-1]
-            if ap_ssid != '':
-                ap_array.append(ap_ssid)
-
-    return ap_array
-
-def create_wpa_supplicant(ssid, wifi_key):
-
-        os.system('nmcli con add ifname wlan0 type wifi ssid ' + ssid + ' wifi-sec.key-mgmt wpa-psk wifi-sec.psk ' + wifi_key + ' ')
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5812)
