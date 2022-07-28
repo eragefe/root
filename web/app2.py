0a6100f8bea8a331edc495a4a35030eb5d32d0fb
eragefe <georgios@gdis.se>
2022-07-28 20:16:52 +0200

    new

--- web/app2.py
+++ web/app2.py
@@ -31,6 +31,11 @@ def volume2():
             vol = f.read()
             return vol
 
+def create_file(a):
+    temp_conf_file = open('/root/vol', 'w')
+    temp_conf_file.write('' + a + '')
+    temp_conf_file.close
+
 @app.route("/volume", methods = ['GET', 'POST'])
 def volume():
     if request.method == 'POST':
@@ -44,6 +49,31 @@ def volume():
         with open("/root/vol", "r") as f:
             vol = f.read()
             return vol
+
+@app.route('/filter', methods = ['GET', 'POST'])
+def filter():
+    if request.method == 'POST':
+        b = request.form["filter"]
+        if b == "nos":
+           os.system('amixer cset numid=5 1 >/dev/nul')
+           os.system('echo "(N.O.S)" > /root/filter')
+        if b == "slow":
+           os.system('amixer cset numid=5 0 >/dev/nul')
+           os.system('amixer cset numid=4 1 >/dev/nul')
+           os.system('echo "(Slow Rolloff)" > /root/filter')
+        if b == "fast":
+           os.system('amixer cset numid=5 0 >/dev/nul')
+           os.system('amixer cset numid=4 0 >/dev/nul')
+           os.system('echo "(Fast Rolloff)" > /root/filter')
+        if b == "min":
+           os.system('amixer cset numid=5 0 >/dev/nul')
+           os.system('amixer cset numid=4 2 >/dev/nul')
+           os.system('echo "(Minimal Phase)" > /root/filter')
+        return redirect('/')
+    else:
+        with open("/root/filter", "r") as f:
+             filter = f.read()
+             return filter
 
 @app.route('/input', methods = ['GET', 'POST'])
 def input():
@@ -78,28 +108,6 @@ def test():
         os.system('bash /root/net')
     if test == "sysupdate":
         return render_template('update.html')
-    return redirect('/')
-
-@app.route('/filter', methods = ['GET', 'POST'])
-def filter():
-    filter = request.form["filter"]
-    if filter == "nos":
-        os.system('amixer cset numid=5 1 >/dev/nul')
-        os.system('echo "(N.O.S)" > /root/filter')
-    if filter == "slow":
-        os.system('amixer cset numid=5 0 >/dev/nul')
-        os.system('amixer cset numid=4 1 >/dev/nul')
-        os.system('echo "(Slow Rolloff)" > /root/filter')
-    if filter == "fast":
-        os.system('amixer cset numid=5 0 >/dev/nul')
-        os.system('amixer cset numid=4 0 >/dev/nul')
-        os.system('echo "(Fast Rolloff)" > /root/filter')
-    if filter == "min":
-        os.system('amixer cset numid=5 0 >/dev/nul')
-        os.system('amixer cset numid=4 2 >/dev/nul')
-        os.system('echo "(Minimal Phase)" > /root/filter')
-    with open("/root/filter", "r") as f:
-         filter = f.read()
     return redirect('/')
 
 @app.route('/power')
@@ -145,10 +153,5 @@ def next():
     os.system('mpc next')
     return redirect('/')
 
-def create_file(a):
-    temp_conf_file = open('/root/vol', 'w')
-    temp_conf_file.write('' + a + '')
-    temp_conf_file.close
-    
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5812)
